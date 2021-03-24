ec880fc90a849409bdf932fdd1527df3c946393b
alancdias <81328473+alancdias@users.noreply.github.com>
2021-03-24 17:04:29 -0300

    Add files via upload

---
+++ 1to100withnodigits.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+'''Program that print numbers from 1 to 100,
+but the code have no digits.
+Made for a challenge.'''
+from math import pi, e;
+counter = False; #initializes a counter with 0 (False)
+goal = e*pi**pi; #this is almost 100 and will serve as stop point
+while (counter<goal):
+  counter += True; #increases the counter by 1 (True)
+  print(counter);
+
